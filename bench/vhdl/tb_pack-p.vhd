b4dc2400cb7add53b5aed7fdf7cb496e87d1279c
arniml <arniml@0a032e78-e3ad-49bf-a5a3-99ed41596107>
2005-03-08 22:06:39 +0000

    added integer->string conversion function

--- bench/vhdl/tb_pack-p.vhd
+++ bench/vhdl/tb_pack-p.vhd
@@ -2,7 +2,7 @@
 --
 -- SD/MMC Bootloader
 --
--- $Id: tb_pack-p.vhd,v 1.1 2005-02-08 21:09:20 arniml Exp $
+-- $Id: tb_pack-p.vhd,v 1.2 2005-03-08 22:06:39 arniml Exp $
 --
 -------------------------------------------------------------------------------
 
@@ -14,6 +14,8 @@ package tb_pack is
 
   function calc_crc(payload : in std_logic_vector) return std_logic_vector;
   function calc_crc(payload : in unsigned) return unsigned;
+
+  function to_string(value : in integer) return string;
 
 end tb_pack;
 
@@ -46,6 +48,35 @@ package body tb_pack is
     return unsigned(calc_crc(std_logic_vector(payload)));
   end calc_crc;
 
+  function to_string(value : in integer) return string is
+    variable str: string (11 downto 1);
+    variable val: integer := value;
+    variable digit: natural;
+    variable index: natural := 0;
+  begin
+    -- Taken from:
+    --  textio package body.  This file is part of GHDL.
+    --  Copyright (C) 2002 Tristan Gingold.
+    --  Note: the absolute value of VAL cannot be directly taken, since
+    --  it may be greather that the maximum value of an INTEGER.
+    loop
+      --  LRM93 7.2.6
+      --  (A rem B) has the sign of A and an absolute value less then
+      --   the absoulte value of B.
+      digit := abs (val rem 10);
+      val := val / 10;
+      index := index + 1;
+      str (index) := character'val(48 + digit);
+      exit when val = 0;
+    end loop;
+    if value < 0 then
+      index := index + 1;
+      str(index) := '-';
+    end if;
+
+    return str;
+  end to_string;
+
 end tb_pack;
 
 
@@ -53,4 +84,7 @@ end tb_pack;
 -- File History:
 --
 -- $Log: not supported by cvs2svn $
+-- Revision 1.1  2005/02/08 21:09:20  arniml
+-- initial check-in
+--
 -------------------------------------------------------------------------------
